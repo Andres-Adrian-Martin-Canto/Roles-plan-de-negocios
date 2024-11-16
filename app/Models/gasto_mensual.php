ca05d8332e3a7ee3bb015519cb401a7acebe7ecb
Andres-Adrian-Martin-Canto <L19390732@chetumal.tecnm.mx>
2024-11-16 15:01:10 -0500

    CREATE: file new for gastos_mensuales

---
+++ app/Models/gasto_mensual.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class gasto_mensual extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'unidad',
+        'monto_unitario'
+    ];
+
+    protected $table = 'gastos_mensuales';
+
+    // * Relaciones
+}
