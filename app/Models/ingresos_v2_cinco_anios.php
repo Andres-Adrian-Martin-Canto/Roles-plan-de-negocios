4f6e6f83a3b0d710a12e868e54a2949222980031
Andres-Adrian-Martin-Canto <L19390732@chetumal.tecnm.mx>
2024-11-24 13:57:29 -0500

    CREATE: File for ingresos_v2_cinco_anios, migration and model

---
+++ app/Models/ingresos_v2_cinco_anios.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ingresos_v2_cinco_anios extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id_ingresos_anuales',
+        'estudio_id',
+        'anio',
+        'monto'
+    ];
+
+    protected $table = "ingresos_anuales_v2";
+}
