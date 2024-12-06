9ed715a6318cf41aa3425f808d4e7abced57f175
Andres-Adrian-Martin-Canto <L19390732@chetumal.tecnm.mx>
2024-12-06 17:56:11 -0500

    CREATE: Boton de regreso

--- resources/views/plan_financiero/inversionInicial.blade.php
+++ resources/views/plan_financiero/inversionInicial.blade.php
@@ -13,6 +13,20 @@
 <body class="m-0 p-0">
     {{-- Footer  --}}
     @include('layouts.navigation')
+
+    {{-- Boton de regreso --}}
+    <div>
+        <button class="bg-slate-300 text-black hover:bg-green-400 m-2 rounded-sm">
+            <a href="{{ route('plan_de_negocio.gastoPreoperativo.index', $plan_de_negocio) }}" class="flex gap-3 p-1">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
+                </svg>
+                <span class="">Regresar</span>
+            </a>
+        </button>
+    </div>
     {{-- Cabeza de la pagina --}}
     <div class="flex items-center flex-col text-black my-2 sm:my-4">
         <h1 class="text-4xl antialiased font-sans ">Plan Financiero</h1>
