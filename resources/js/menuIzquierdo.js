ddb38cc35f973df3c5cb5577af8551a50f881b7a
Andres-Adrian-Martin-Canto <L19390732@chetumal.tecnm.mx>
2024-12-15 12:54:39 -0500

    Style: menu-izquierdo responsive para laptop

--- resources/js/menuIzquierdo.js
+++ resources/js/menuIzquierdo.js
@@ -6,13 +6,13 @@ document.addEventListener("DOMContentLoaded", function () {
         ae.onmouseenter = function () {
             ae.querySelector('span').classList.replace('bg-gray-700', 'bg-green-500');
             ae.querySelector('h3').classList.replace('text-gray-400', 'text-white');
-            ae.querySelector('svg').classList.replace('dark:text-gray-400', 'dark:text-white');
+            ae.querySelector('svg').classList.replace('text-gray-400', 'text-white');
         };
 
         ae.onmouseleave = function () {
             ae.querySelector('span').classList.replace('bg-green-500', 'bg-gray-700');
             ae.querySelector('h3').classList.replace('text-white', 'text-gray-400');
-            ae.querySelector('svg').classList.replace('dark:text-white', 'dark:text-gray-400');
+            ae.querySelector('svg').classList.replace('text-white', 'text-gray-400');
         };
     });
 });
